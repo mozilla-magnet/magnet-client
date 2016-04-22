caa1f569d7fea0676cc872cfd6c5b410033d9871
Sam Giles <sam.e.giles@gmail.com>
2016-04-22 15:37:41 +0100

    Use github API to create tag

--- build-scripts/tag-nightly.sh
+++ build-scripts/tag-nightly.sh
@@ -6,11 +6,6 @@ if [[ "$NIGHTLY" != "true" ]];then
     exit
 fi
 
-echo "Tagging nightly"
-git config --global user.email "builds@travis-ci.com"
-git config --global user.name  "Travis CI"
-
-
 if [[ "$BUILD_TYPE" != "android" && "$BUILD_TYPE" != "ios" ]]; then
     echo "Invalid BUILD_TYPE, should be 'android' or 'ios'"
     exit -1
@@ -18,7 +13,41 @@ else
     export TAG=nightly-${BUILD_TYPE}
 fi
 
+function github_post() {
+    # TODO: Use a special account
+    curl -s -X POST \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/vnd.github.v3+json" \
+        -d "$2" \
+        -u samgiles:${GITHUB_AUTH_TOKEN} \
+        https://api.github.com$1
+}
 
-# Force new 'git tag'
-git tag $TAG -a -m "Nightly - ${BUILD_TYPE}" -f
-git push --tags
+
+# POST /repos/:owner/:repo/git/tags
+timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+body="{
+  \"tag\":\"${TAG}\",
+  \"message\":\"${BUILD_TYPE} nightly - ${timestamp}\",
+  \"object\":\"${TRAVIS_COMMIT}\",
+  \"type\":\"commit\",
+  \"tagger\": {
+    \"name\":\"Travis CI\",
+    \"email\":\"nobody@mozilla.com\",
+    \"data\":\"${timestamp}\"
+  }
+}"
+
+response=$(github_post /repos/mozilla-magnet/magnet/git/tags "$body")
+
+# Super hack to get the tag reference out of the json response, this may break
+# if the API response format changes (install a real json parser?)
+TAG_REF=$(echo $response || perl -pe 's/"sha"://; s/^"//; s/",$//; s/"//; s/",//;' | cut -f3 -d" ")
+
+body="{
+  \"ref\":\"refs/tags/${TAG}\",
+  \"sha\":\"${TRAVIS_COMMIT}\"
+}"
+
+github_post /repos/mozilla-magnet/magnet/git/refs "$body"
