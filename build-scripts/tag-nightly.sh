906d742fdbddbc7b6b69b3868be5b71d0f088f88
Sam Giles <sam.e.giles@gmail.com>
2016-04-22 13:33:15 +0100

    Tag nightly builds

---
+++ build-scripts/tag-nightly.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Exit early and don't tag if not building a nightly build
+if [[ "$NIGHTLY" != "true" ]];then
+    exit
+fi
+
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name  "Travis CI"
+
+
+if [[ "$BUILD_TYPE" != "android" && "$BUILD_TYPE" != "ios" ]]; then
+    echo "Invalid BUILD_TYPE, should be 'android' or 'ios'"
+    exit -1
+else
+    export TAG=nightly-${BUILD_TYPE}
+fi
+
+# Force new 'git tag'
+git tag $TAG -a -m "Nightly - ${BUILD_TYPE}" -f
+git push --tags
