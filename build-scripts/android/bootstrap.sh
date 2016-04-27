50fb810a0e5a4802d8425ced9b2ccc75490d9b08
Sam Giles <sam.e.giles@gmail.com>
2016-04-27 17:41:24 +0100

    Set up APK signing

--- build-scripts/android/bootstrap.sh
+++ build-scripts/android/bootstrap.sh
@@ -3,7 +3,7 @@
 brew reinstall android-sdk
 
 export ANDROID_HOME=$(brew --prefix android-sdk)
-export ARTIFACT=android/app/build/outputs/apk/app-debug.apk
+export ARTIFACT=android/app/build/outputs/apk/app-release.apk
 
 android=${ANDROID_HOME}/tools/android
 
@@ -12,3 +12,13 @@ android=${ANDROID_HOME}/tools/android
 
 # Ensure android packages are installed
 ./build-scripts/android/accept-license.expect "./build-scripts/android/install-android-packages.sh"
+
+# Create APK signing variables
+
+# Get the release keystore from the keystore service
+wget https://${TOKEN_SERVICE_USER}:${TOKEN_SERVICE_AUTH_TOKEN}@ci.tengam.org/keystore/magnet-release.keystore
+
+echo "RELEASE_STORE_FILE=`pwd`/magnet-release.keystore" > ~/.gradle/gradle.properties
+echo "RELEASE_KEY_ALIAS=magnet-release" >> ~/.gradle/gradle.properties
+echo "RELEASE_KEY_PASSWORD=${RELEASE_KEY_PASSWORD}" >> ~/.gradle/gradle.properties
+echo "RELEASE_STORE_PASSWORD=${RELEASE_STORE_PASSWORD}" >> ~/.gradle/gradle.properties
