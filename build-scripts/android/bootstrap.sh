69cad3488172f3ca8b9497506ab952ee2db79e93
Sam Giles <sam.e.giles@gmail.com>
2016-04-27 18:12:40 +0100

    Make sure the gradle directory exists

--- build-scripts/android/bootstrap.sh
+++ build-scripts/android/bootstrap.sh
@@ -18,6 +18,7 @@ android=${ANDROID_HOME}/tools/android
 # Get the release keystore from the keystore service
 wget https://${TOKEN_SERVICE_USER}:${TOKEN_SERVICE_AUTH_TOKEN}@ci.tengam.org/keystore/magnet-release.keystore
 
+mkdir -p ~/.gradle
 echo "RELEASE_STORE_FILE=`pwd`/magnet-release.keystore" > ~/.gradle/gradle.properties
 echo "RELEASE_KEY_ALIAS=magnet-release" >> ~/.gradle/gradle.properties
 echo "RELEASE_KEY_PASSWORD=${RELEASE_KEY_PASSWORD}" >> ~/.gradle/gradle.properties
