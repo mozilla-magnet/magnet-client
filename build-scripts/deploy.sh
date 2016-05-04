99206405c68e62bb5a2c3c5a88d8c33fbf459e0e
Sam Giles <sam.e.giles@gmail.com>
2016-05-04 14:24:42 +0100

    Deploy on semver tags too

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $TAG = "nightly" ]]; then
+if [[ $TAG = "nightly" || $TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     ./build-scripts/deploy-github.js
 else
     echo "Skipping deploy, not a release build..."
