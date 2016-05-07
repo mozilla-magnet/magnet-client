fe091a32047689113859ba3d5f8c45db54c13774
Francisco Jordano <fjordano@mozilla.com>
2016-05-07 20:40:23 +0100

    Making the container full screen

--- index.android.js
+++ index.android.js
@@ -1,34 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 var ListView = require('./lib/views/list');
 var React = require('react-native');
 
 var {
-  AppRegistry,
   StyleSheet,
   Component,
   UIManager,
-  Platform,
-  View,
-  StatusBar
+  AppRegistry
 } = React;
 
 class App extends Component {
   render() {
-    return (<View>
-        <StatusBar
-          hidden={statusBar.hidden}/>
-        <ListView style={styles.list}/>
-        </View>);
+    return  <ListView style={styles.list}/>;
   }
 }
-
-const statusBar = {
-  hidden: true
-};
 
 const styles = StyleSheet.create({
   list: {
@@ -37,8 +21,6 @@ const styles = StyleSheet.create({
 });
 
 // layout-animations are turned off on android by default
-if (Platform.OS === 'android') {
-  UIManager.setLayoutAnimationEnabledExperimental(true);
-}
+UIManager.setLayoutAnimationEnabledExperimental(true);
 
 AppRegistry.registerComponent('Magnet', () => App);
--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,32 @@
-require('./index.android.js');
+var ListView = require('./lib/views/list');
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Component,
+  View,
+  StatusBar
+} = React;
+
+class App extends Component {
+  render() {
+    return  (
+      <View style={styles.container}>
+        <ListView style={styles.list}/>
+      </View>);
+  }
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#f2f2f2'
+  }
+});
+
+AppRegistry.registerComponent('Magnet', () => App);
