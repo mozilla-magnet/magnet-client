e01edcb896b0ac1c31b7e361ab6b91cf0d29c767
Sam Giles <sam.e.giles@gmail.com>
2016-05-13 15:36:23 +0100

    Clean gradle output please

--- build-scripts/android/build.sh
+++ build-scripts/android/build.sh
@@ -5,8 +5,8 @@ cd android
 ./gradlew check
 
 if [[ $BUILD_RELEASE == 1 ]]; then
-    ./gradlew assembleRelease --info | tee
+    ./gradlew assembleRelease --info --console plain | tee
 else
-    ./gradlew build --info | tee
+    ./gradlew build --info --console plain | tee
 fi
 cd -
