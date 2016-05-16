1c473ff27073a5c6f2f707cbe1a10b0f38660751
Sam Giles <sam.e.giles@gmail.com>
2016-05-17 00:56:29 +0100

    Bootstrap android

--- build-scripts/bootstrap.sh
+++ build-scripts/bootstrap.sh
@@ -16,3 +16,12 @@ npm install -g react-native-cli
 npm install
 
 export PATH=$PATH:./node_modules/.bin
+
+if [[ "$BUILD_TYPE" == "android" ]];then
+    echo "Bootstrapping for android"
+    source ./build-scripts/android/bootstrap.sh
+elif [[ "$BUILD_TYPE" == "ios" ]]; then
+    echo "Bootstrapping for ios"
+else
+    echo "Unknown build type"
+fi
