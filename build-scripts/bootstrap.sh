74e19db9197056311a9fb06a2855305953fc3b1c
Sam Giles <sam.e.giles@gmail.com>
2016-05-17 00:28:35 +0100

    Assign the tag before using it

--- build-scripts/bootstrap.sh
+++ build-scripts/bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# copy the TRAVIS_TAG to the more generic 'TAG' var
+export TAG=$TRAVIS_TAG
+
 if [[ $TAG = "nightly" || $TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     export BUILD_RELEASE=1
 else
     export BUILD_RELEASE=0
 fi
-
-# copy the TRAVIS_TAG to the more generic 'TAG' var
-export TAG=$TRAVIS_TAG
 
 npm install -g react-native-cli
 
