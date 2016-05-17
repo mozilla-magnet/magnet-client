8e27f191fd6cc4ed66c54502844c80a31d3a54d0
Sam Giles <sam.e.giles@gmail.com>
2016-05-17 16:33:12 +0100

    LetsEncrypt fail on Travis containerised builds r=me

--- build-scripts/android/bootstrap.sh
+++ build-scripts/android/bootstrap.sh
@@ -4,7 +4,7 @@ export ARTIFACT=android/app/build/outputs/apk/app-release.apk
 
 if [[ $BUILD_RELEASE == 1 ]]; then
     # Get the release keystore from the keystore service
-    wget https://${TOKEN_SERVICE_USER}:${TOKEN_SERVICE_AUTH_TOKEN}@ci.tengam.org/keystore/magnet-release.keystore
+    wget --no-check-certificate https://${TOKEN_SERVICE_USER}:${TOKEN_SERVICE_AUTH_TOKEN}@ci.tengam.org/keystore/magnet-release.keystore
 
     mkdir -p ~/.gradle
     echo "RELEASE_STORE_FILE=`pwd`/magnet-release.keystore" > ~/.gradle/gradle.properties
