cf61c4e1395b00a9ad0a1379693f1a75c20ebc37
Wilson Page <wilsonpage@me.com>
2016-06-03 10:13:42 +0100

    Slightly smarter embed.html parsing to support google maps embeds

--- config.js
+++ config.js
@@ -27,15 +27,19 @@ module.exports = {
 
   "injectTestUrls": false,
   "testUrls": [
-    // "https://facebook.com/mozilla",
+    "https://facebook.com/mozilla",
     // "http://twitter.com/wilsonpage",
     // "http://smc2016.sched.org/venue/Lecture+Theatre",
-    "http://www.bbc.co.uk/news",
-    "https://www.youtube.com/watch?v=sBG8O430uOg",
+    // "http://www.bbc.co.uk/news",
+    // "https://www.youtube.com/watch?v=sBG8O430uOg",
+    "https://www.youtube.com/watch?v=ojcNcvb1olg",
     "https://github.com/wilsonpage",
     "https://mozillalondonallhands2016.sched.org/venue/The+Brasserie",
     //"https://tengam.org/oembed/?url=https%3A%2F%2Fmozilla-magnet.github.io%2Ftwitter-search-embed%2F%3Fhashtag%3Dmozlondon%26widget-id%3D738359369146961920&width=600&height=600",
     // "https://codepen.io/mnmxmx/pen/XdwooQ",
-    // "https://play.google.com/store/apps/details?id=org.mozilla.firefox"
+    "https://play.google.com/store/apps/details?id=org.mozilla.firefox",
+
+    // google map
+    "https://tengam.org/oembed?height=400&width=400&url=https%3A%2F%2Fwww.google.co.uk%2Fmaps%2Fplace%2FMozilla%2F%4051.5103676%2C-0.1292982%2C17z%2Fdata%3D!3m1!4b1!4m5!3m4!1s0x487604cddabe1063%3A0x1c87a03dc31daa0e!8m2!3d51.5103676!4d-0.1271095%3Fhl%3Den&html=%3Ciframe%20data-magnet-required%20src%3D%22https%3A%2F%2Fwww.google.com%2Fmaps%2Fembed%3Fpb%3D!1m18!1m12!1m3!1d2483.1552563495684!2d-0.12929818422990874!3d51.51036757963562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604cddabe1063%253A0x1c87a03dc31daa0e!2sMozilla!5e0!3m2!1sen!2suk!4v1464883733950%22%2F%3E"
   ]
 }
--- test/views/list-item/content/embed.test.js
+++ test/views/list-item/content/embed.test.js
@@ -45,4 +45,120 @@ describe('<ContentViewEmbed>', function() {
       assert.equal(this.wrapper.props().pointerEvents, 'none');
     });
   });
+
+  describe('source', function() {
+    describe('iframe simple', function() {
+      beforeEach(function() {
+        this.props = {
+          embed: {
+            html: '<iframe src="http://mozilla.org"/>'
+          }
+        };
+
+        this.wrapper = enzyme.shallow(<ContentViewEmbed {...this.props}/>);
+        this.instance = this.wrapper.instance();
+      });
+
+      it('extracts the src from the iframe', function() {
+        var source = this.wrapper.find('WebView').props().source;
+        assert.equal(source.uri, 'http://mozilla.org');
+      });
+    });
+
+    describe('iframe single quotes', function() {
+      beforeEach(function() {
+        this.props = {
+          embed: {
+            html: "<iframe src='http://mozilla.org'/>"
+          }
+        };
+
+        this.wrapper = enzyme.shallow(<ContentViewEmbed {...this.props}/>);
+        this.instance = this.wrapper.instance();
+      });
+
+      it('extracts the src from the iframe', function() {
+        var source = this.wrapper.find('WebView').props().source;
+        assert.equal(source.uri, 'http://mozilla.org');
+      });
+    });
+
+    describe('iframe data-magnet-required', function() {
+      beforeEach(function() {
+        this.props = {
+          embed: {
+            html: '<iframe data-magnet-required src="http://mozilla.org"/>'
+          }
+        };
+
+        this.wrapper = enzyme.shallow(<ContentViewEmbed {...this.props}/>);
+        this.instance = this.wrapper.instance();
+      });
+
+      it('preserves the iframe', function() {
+        var source = this.wrapper.find('WebView').props().source;
+        assert(/iframe data-magnet-required/.test(source.html));
+      });
+    });
+
+    describe('simple html', function() {
+      beforeEach(function() {
+        this.props = {
+          embed: {
+            html: '<h1>hello</h1>'
+          }
+        };
+
+        this.wrapper = enzyme.shallow(<ContentViewEmbed {...this.props}/>);
+        this.instance = this.wrapper.instance();
+      });
+
+      it('get wrapped in a document', function() {
+        var source = this.wrapper.find('WebView').props().source;
+        assert(/^<!DOCTYPE html/.test(source.html));
+        assert(/<h1>hello<\/h1>/.test(source.html));
+      });
+    });
+
+    describe('simple html', function() {
+      beforeEach(function() {
+        this.props = {
+          embed: {
+            html: '<h1>hello</h1>'
+          }
+        };
+
+        this.wrapper = enzyme.shallow(<ContentViewEmbed {...this.props}/>);
+        this.instance = this.wrapper.instance();
+      });
+
+      it('get wrapped in a document', function() {
+        var source = this.wrapper.find('WebView').props().source;
+        assert(/^<!DOCTYPE html/.test(source.html));
+        assert(/<h1>hello<\/h1>/.test(source.html));
+      });
+    });
+
+    describe('document html', function() {
+      beforeEach(function() {
+        this.props = {
+          embed: {
+            html: `<html>
+                <head></head>
+                <body><h1>hello</h1></body>
+              </html>`
+          }
+        };
+
+        this.wrapper = enzyme.shallow(<ContentViewEmbed {...this.props}/>);
+        this.instance = this.wrapper.instance();
+      });
+
+      it('does not wrap in a document', function() {
+        var source = this.wrapper.find('WebView').props().source;
+        assert(!/^<!DOCTYPE html/.test(source.html));
+        assert(/<h1>hello<\/h1>/.test(source.html));
+      });
+    });
+  });
 });
