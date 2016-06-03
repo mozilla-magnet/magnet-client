f9ef39a34d8c9409cf3b34ce40f6b88aa93852b3
Francisco Jordano <fjordano@mozilla.com>
2016-06-03 12:02:20 +0100

    Adding metrics: - Installation - Launch - Expand view - Swipe away

--- android/app/src/main/java/org/mozilla/magnet/MainActivity.java
+++ android/app/src/main/java/org/mozilla/magnet/MainActivity.java
@@ -7,6 +7,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -34,6 +36,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
+            new RNDeviceInfo(),
             new MainReactPackage(),
                 new MyAppPackage()
         );
