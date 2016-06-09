88333917efdbc733d5fb6fbfd0d8eeb8002369c6
Samuel Giles <samgiles@users.noreply.github.com>
2016-06-09 19:50:23 +0100

    We need to force a size changed event _after_ layout so that the chrome web view has the correct size. (#148)
    * We need to force a size changed event _after_ layout so that the chrome web view has the correct size.
    
    * Don't need this
    
    * Call layout after super, and rename to the right things..
    
    * Don't need this either

--- android/app/src/main/java/org/mozilla/magnet/webview/MagnetWebView.java
+++ android/app/src/main/java/org/mozilla/magnet/webview/MagnetWebView.java
@@ -17,6 +17,11 @@ public class MagnetWebView extends WebView implements LifecycleEventListener {
     private static final String TAG = "MagnetWebView";
     private boolean layoutSet;
 
+    // Record the last seen size changed event so we can update the webview content target,
+    // after layout
+    private int mLastSeenWidth = -1;
+    private int mLastSeenHeight = -1;
+
     /**
      * WebView must be created with an context of the current activity
      *
@@ -61,18 +66,38 @@ public class MagnetWebView extends WebView implements LifecycleEventListener {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
         Log.d(TAG, "on layout");
+        super.onLayout(changed, left, top, right, bottom);
         setLayout();
+
+        // Force size change AFTER layout, see `onSizeChanged`
+        forceSizeChanged();
     }
 
     // prevent html document from collapsing
     private void setLayout() {
-        if (layoutSet) return;
         getLayoutParams().width = LayoutParams.MATCH_PARENT;
         getLayoutParams().height = LayoutParams.MATCH_PARENT;
+    }
 
-        layoutSet = true;
+    public void forceSizeChanged() {
+        // If the height and width have been set by onSizeChanged already,
+        // i.e. are non-negative, call onSizeChanged again
+        if (mLastSeenHeight >= 0 && mLastSeenWidth >= 0) {
+            onSizeChanged(mLastSeenWidth, mLastSeenHeight, 0, 0);
+        }
+    }
+
+    @Override
+    protected void  onSizeChanged(int aWidth, int aHeight, int aOldWidth, int aOldHeight) {
+
+        // Record the last reported size, we then need to use this to force a
+        // size change AFTER onLayout. This then updates the underlying Chrome
+        // content target.
+        mLastSeenWidth = aWidth;
+        mLastSeenHeight = aHeight;
+
+        super.onSizeChanged(aWidth, aHeight, aOldWidth, aOldHeight);
     }
 
     public void dispatchEvent(String name, WritableMap event) {
