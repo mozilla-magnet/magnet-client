d769eb874884c6895afd2c1c38ff2018df2ac0b5
isabelrios <isabelrios@gmail.com>
2016-06-16 11:38:44 +0100

    building release build

--- test-scripts/test.sh
+++ test-scripts/test.sh
@@ -2,7 +2,7 @@
 set -e
 # Build release = 1 if the tests will be launched only with the release build
 # Setting to 0 for the test setting up the environment
-if [[ $BUILD_RELEASE == 0 ]]; then
+if [[ $BUILD_RELEASE == 1 ]]; then
     source ./test-scripts/launch_tests.sh
 else
     echo "Skipping ui tests, not a release build..."
