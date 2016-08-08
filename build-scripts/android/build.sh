c9e2e645f7942a41d71db672fd9ed270253345c2
Wilson Page <wilsonpage@me.com>
2016-08-08 16:11:21 +0100

    Try to get better output from travis build failure

--- build-scripts/android/build.sh
+++ build-scripts/android/build.sh
@@ -5,7 +5,7 @@ cd android
 ./gradlew check
 
 if [[ $BUILD_RELEASE == 1 ]]; then
-    ./gradlew assembleRelease --info --console plain | tee
+    ./gradlew assembleRelease --stacktrace
 else
     ./gradlew build --info --console plain | tee
 fi
