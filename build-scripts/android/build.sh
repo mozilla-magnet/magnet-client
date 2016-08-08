4053193df2339e6ff7c9294cc05325b5343f9e3a
Wilson Page <wilsonpage@me.com>
2016-08-08 18:57:36 +0100

    Change to only check app source, not our dependencies

--- build-scripts/android/build.sh
+++ build-scripts/android/build.sh
@@ -2,10 +2,10 @@
 
 set -e
 cd android
-./gradlew check
+./gradlew app:check
 
 if [[ $BUILD_RELEASE == 1 ]]; then
-    ./gradlew assembleRelease --stacktrace
+    ./gradlew assembleRelease --info --console plain | tee
 else
     ./gradlew build --info --console plain | tee
 fi
