3ac3cd32cd4e1976b3fd8de55f974eab7856252f
Wilson Page <wilsonpage@me.com>
2016-08-09 11:51:41 +0100

    Add some comments

--- build-scripts/android/build.sh
+++ build-scripts/android/build.sh
@@ -2,11 +2,15 @@
 
 set -e
 cd android
+
+# check our app source code
 ./gradlew app:check
 
 if [[ $BUILD_RELEASE == 1 ]]; then
     ./gradlew assembleRelease --info --console plain | tee
 else
+    # exclude lint task as already done above
     ./gradlew build -x lint --info --console plain | tee
 fi
+
 cd -
