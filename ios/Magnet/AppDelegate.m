d84b274e8f087e58daa2cd0e66906c377f5490df
Wilson Page <wilsonpage@me.com>
2016-10-27 19:17:30 +0100

    Revert back to using Debug default builds (ios)

--- ios/Magnet/AppDelegate.m
+++ ios/Magnet/AppDelegate.m
@@ -20,15 +20,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
-//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
   
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"Magnet"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+                                              moduleName:@"Magnet"
+                                              initialProperties:nil
+                                              launchOptions:launchOptions];
+  
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
