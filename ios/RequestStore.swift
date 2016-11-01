790b96c44921bc5f07f0f4709135c5d34dfa85a4
Francisco Jordano <fjordano@mozilla.com>
2016-11-01 13:59:40 +0000

    Adding json request store

---
+++ ios/RequestStore.swift
@@ -0,0 +1,42 @@
+//
+//  RequestStore.swift
+//  Magnet
+//
+//  Created by Francisco Jordano on 01/11/2016.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class RequestStore {
+  private static var sharedInstance: RequestStore?
+  private static let lockQueue = dispatch_queue_create("com.mozilla.magnet.requeststore", nil)
+  private let db = RequestStoreSQLite()
+  
+  private init() {
+    
+  }
+  
+  class func getInstance() -> RequestStore {
+    dispatch_async(lockQueue) { 
+      if (sharedInstance == nil) {
+        sharedInstance = RequestStore()
+      }
+    }
+    
+    return sharedInstance!
+  }
+  
+  func getJSON(url: String) -> JSON? {
+    return db.get(url)
+  }
+  
+  func setJSON(url: String, value: JSON) {
+    db.put(url, _value: value, _timestamp: nil)
+  }
+  
+  func setJSON(url: String, value: String) {
+    db.put(url, _value: JSON(value), _timestamp: nil)
+  }
+}
---
+++ ios/RequestStoreSQLite.swift
@@ -0,0 +1,76 @@
+//
+//  RequestStoreSQLite.swift
+//  Magnet
+//
+//  Created by Francisco Jordano on 01/11/2016.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+import Foundation
+import SQLite
+import SwiftyJSON
+
+class RequestStoreSQLite {
+  private var db: Connection
+  private let requestStore: Table = Table("requeststore")
+  private let url: Expression<String> = Expression<String>("url")
+  private let value: Expression<String> = Expression<String>("value")
+  private let timestamp: Expression<Int64> = Expression<Int64>("timestamp")
+  
+  init() {
+    let path = NSSearchPathForDirectoriesInDomains(
+      .DocumentDirectory, .UserDomainMask, true
+      ).first!
+    
+    db = try! Connection("\(path)/requeststore.sqlite3")
+    
+    try! db.run(requestStore.create(ifNotExists: true) { t in
+      t.column(url, primaryKey: true)
+      t.column(value)
+      t.column(timestamp)
+    })
+  }
+  
+  func clear() {
+    try! db.run(requestStore.delete())
+  }
+  
+  func put(_url: String, _value: JSON, _timestamp: Int64?) {
+    let stringValue = _value.rawString()!
+    
+    var time = Int64(NSDate().timeIntervalSince1970)
+    if (_timestamp != nil) {
+      time = _timestamp!
+    }
+    
+    try! db.transaction(block: {
+      let filteredResults = self.requestStore.filter(self.url == _url)
+      
+      if try! self.db.run(filteredResults.update(self.value <- stringValue, self.timestamp <- time)) > 0 {
+        // Updated correctly
+      } else {
+        // Insert
+        let insert = self.requestStore.insert(self.url <- _url, self.value <- stringValue, self.timestamp <- time)
+        try! self.db.run(insert)
+      }
+    })
+  }
+  
+  func delete(_url: String) {
+    let item = requestStore.filter(url == _url)
+    try! db.run(item.delete())
+  }
+  
+  func get(_url: String) -> JSON? {
+    let query = requestStore.filter(url == _url).limit(1)
+    
+    let result = Array(try! db.prepare(query));
+    
+    guard result.count == 1 else {
+      return nil
+    }
+    
+    return JSON(result[0][value])
+  }
+
+}
