cb086116269b2e5e094f92f2fc79f13e1f7bb8d3
Francisco Jordano <fjordano@mozilla.com>
2016-11-01 14:38:50 +0000

    Base api for ios

---
+++ ios/Api.swift
@@ -0,0 +1,82 @@
+//
+//  Api.swift
+//  Magnet
+//
+//  Created by Francisco Jordano on 01/11/2016.
+//  Copyright © 2016 Mozilla. All rights reserved.
+//
+
+import Foundation
+
+protocol Api {
+  func get(path: String, callback: ApiCallback)
+  func post(path: String, data: AnyObject, callback: ApiCallback)
+  func put(path: String, data: AnyObject, callback: ApiCallback)
+  func delete(path: String, callback: ApiCallback)
+}
+
+class ApiCallback {
+  var onSuccess: ((AnyObject) -> Void)
+  var onError: ((AnyObject) -> Void)
+  
+  init(success: ((AnyObject) -> Void), error: ((AnyObject) -> Void)) {
+    onSuccess = success
+    onError = error
+  }
+}
+
+class ApiBase: Api {
+  private var endpoints: Dictionary<String, Api> = Dictionary<String, Api>()
+  
+  func mount(path: String, api: Api) {
+    endpoints[path] = api
+  }
+  
+  private func find(namespace: String) -> Api? {
+    return endpoints[namespace]
+  }
+  
+  func get(path: String, callback: ApiCallback) {
+    let api = find(path)
+    
+    guard api != nil else {
+      callback.onError("Could not find route \(path)")
+      return
+    }
+    
+    api.get(path, callback)
+  }
+  
+  func post(path: String, data: AnyObject, callback: ApiCallback) {
+    let api = find(path)
+    
+    guard api != nil else {
+      callback.onError("Could not find route \(path)")
+      return
+    }
+    
+    api.post(path, callback)
+  }
+  
+  func put(path: String, data: AnyObject, callback: ApiCallback) {
+    let api = find(path)
+    
+    guard api != nil else {
+      callback.onError("Could not find route \(path)")
+      return
+    }
+    
+    api.put(path, callback)
+  }
+  
+  func delete(path: String, callback: ApiCallback) {
+    let api = find(path)
+    
+    guard api != nil else {
+      callback.onError("Could not find route \(path)")
+      return
+    }
+    
+    api.delete(path, callback)
+  }
+}
