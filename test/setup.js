3d823ce6859e8b0ecbcb3065f622dc3b02393950
Wilson Page <wilsonpage@me.com>
2016-11-15 17:47:02 +0000

    Fix tests

---
+++ test/setup.js
@@ -0,0 +1,11 @@
+
+// var ReactNative = require('react-native');
+// ReactNative.NativeModules.GoogleAnalyticsBridge = {};
+// ReactNative.NativeModules.GoogleAnalyticsBridge = {};
+//
+
+// try {
+//   console.log('xxx', jest.genMockFromModule('react-native-google-analytics-bridge'));
+// } catch(e) {
+//   console.log('eee', e);
+// }
