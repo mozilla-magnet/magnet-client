7b214acf81666e7803f8605963e38ef838edcd8e
Francisco Jordano <fjordano@mozilla.com>
2016-11-23 15:10:37 +0000

    Adding spinner while the image loads

--- ios/HeroNotifications/NotificationViewController.swift
+++ ios/HeroNotifications/NotificationViewController.swift
@@ -13,6 +13,7 @@ import UserNotificationsUI
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var loadingSpinner: UIActivityIndicatorView!
   
     private static var notification: UNNotification?
   
@@ -22,14 +23,12 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
   
     func didReceiveNotification(notification: UNNotification) {
       NotificationViewController.notification = notification
+
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        self.processNotification()
+      })
+
       
-      let notifyTimer = NSTimer.init(
-        timeInterval: 1,
-        target: self,
-        selector: #selector(processNotification),
-        userInfo: nil,
-        repeats: false)
-      NSRunLoop.mainRunLoop().addTimer(notifyTimer, forMode: NSDefaultRunLoopMode)
     }
   
     func processNotification() {
@@ -41,17 +40,31 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
         return
       }
       
-      guard let url = NSURL(string: imageURL) else {
-        return
-      }
+      // Start animating the spinner that is currently visible
+      self.loadingSpinner.hidden = false
+      self.loadingSpinner.startAnimating()
       
-      guard let image = UIImage(data: try NSData(contentsOfURL: url)!) else {
-        return
-      }
+      self.imageView.stopAnimating()
       
-      self.imageView.image = image
-      self.imageView.clipsToBounds = true
-      self.imageView.contentMode = .ScaleAspectFill
+      NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: imageURL)!, completionHandler: { (data, response, error) -> Void in
+        
+        if error != nil {
+          self.loadingSpinner.stopAnimating()
+          return
+        }
+
+        dispatch_async(dispatch_get_main_queue(), { 
+          let image = UIImage(data: data!)
+          self.imageView.image = image
+          self.imageView.clipsToBounds = true
+          self.imageView.contentMode = .ScaleAspectFill
+          
+          self.loadingSpinner.hidden = true
+          self.loadingSpinner.stopAnimating()
+          self.imageView.hidden = false
+        })
+        
+      }).resume()
     }
   
 }
